99ae8fefaad7bf82c0a66a80a0d3d99f5e874e4a
lawrencenoman-01 <PhinconTraineeOlen@01>
2023-10-30 16:55:39 +0700

    feat(FSB3-5): Create Countdown Timer App
    1. Create countdown app with react and vite js.
    2. Create responsive design for mobile view with max-width: 576px
    3. handle custparam, default date: "2024-07-17:00:00" (doesn't finished)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
-import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,100 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react'
+import '../styles/App.scss'
+import facebook from '../assets/images/icon-facebook.svg'
+import pinterest from '../assets/images/icon-pinterest.svg'
+import instagram from '../assets/images/icon-instagram.svg'
+import { useLocation } from 'react-router-dom'
+
+const Homepage = ({endDate}) => {
+  const location = useLocation()
+  // const searchParams = new URLSearchParams(location.search)
+  // const endDateParams = searchParams.get('date')
+
+  const [countdown, setCountdown] = useState({
+    days: '00',
+    hours: '00',
+    minutes: '00',
+    seconds: '00',
+  })
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date().getTime()
+      const duration = typeof endDate === 'number' ? endDate - now : endDate.getTime() - now
+
+      const days = String(Math.floor(duration / (1000*60*60*24))).padStart(2, '0')
+      const hours = String(Math.floor((duration % (1000*60*60*24)) / (1000 * 60 * 60))).padStart(2, '0')
+      console.log(hours);
+      const minutes = String(Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60) )).padStart(2, '0')
+      const seconds = String(Math.floor((duration % (1000 * 60)) / (1000))).padStart(2, '0')
+
+      setCountdown({
+        days,
+        hours,
+        minutes,
+        seconds
+      })
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [endDate])
+
+  return (
+    <div className='container'>
+      <div className='header'>
+        <h1> We're Launching Soon </h1>
+      </div>
+      <div className='countdown-wrapper'>
+        <div className='countdown-value'>
+          <div className='countdown-time'>
+            <h1> {countdown.days} </h1>
+          </div>
+          <div className='countdown-title'>
+            <p> {Number(countdown.days) > 1 ? 'Days' : 'Day'} </p>
+          </div>
+        </div>
+        <div className='countdown-value'>
+          <div className='countdown-time'>
+            <h1> {countdown.hours}</h1>
+          </div>
+          <div className='countdown-title'>
+            <p> {Number(countdown.hours) > 1 ? 'Hours' : 'Hour'} </p>
+          </div>
+        </div>
+        <div className='countdown-value'>
+          <div className='countdown-time'>
+            <h1> {countdown.minutes} </h1>
+          </div>
+          <div className='countdown-title'>
+            <p> {Number(countdown.minutes) > 1 ? 'Minutes' : 'Minute'} </p>
+          </div>
+        </div>
+        <div className='countdown-value'>
+          <div className='countdown-time'>
+            <h1> {countdown.seconds} </h1>
+          </div>
+          <div className='countdown-title'>
+            <p> {Number(countdown.seconds) > 1 ? 'Seconds' : 'Second'} </p>
+          </div>
+        </div>
+        {/* <Test duration={2 * 24 * 60 * 60 * 1000} /> */}
+      </div>
+      <div className='footer'>
+        <div className='facebook'>
+          <img src={facebook} />
+        </div>
+        <div className='pinterest'>
+          <img src={pinterest} />
+        </div>
+        <div className='instagram'>
+          <img src={instagram} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
